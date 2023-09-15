14f906e3f6bee159cd583e2baae75355bcafd9dc
alanjomy22 <alan.j@keyvalue.systems>
2023-09-16 04:17:18 +0530

    add changes for AI generated workflows

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,10 @@ import {
   useGetAllTemplatesQuery,
   useGetAllWorkflowsQuery,
   useGenerateAiWorkFlowMutation,
+  useGetAvailableModelsQuery,
+  useSaveWorkflowMutation,
 } from "../../api/baseApi";
+import { v4 as uuidv4 } from "uuid";
 import {
   Modal,
   ModalOverlay,
@@ -22,10 +25,13 @@ import { AiOutlineCopy } from "react-icons/ai";
 import { TEMPLATES_LIST } from "../templates-list/TemplatesList";
 import { useState } from "react";
 import "./styles.scss";
+import { MarkerType } from "reactflow";
 
 const Home = () => {
   const { data, error, isLoading } = useGetAllWorkflowsQuery();
+  const { data: modelsList } = useGetAvailableModelsQuery();
   const [getAiWorkflow] = useGenerateAiWorkFlowMutation();
+  const [saveWorkFlow] = useSaveWorkflowMutation();
   const navigate = useNavigate();
   const {
     data: workflows,
@@ -61,10 +67,112 @@ const Home = () => {
     setOpenModal(true);
   };
 
-  const handleSubmitCreate = async () => {
-    const result = await getAiWorkflow({ text: aiPromptData });
-    console.log(result);
+  const getNodesFromAiResponse = (res = "") => {
+    const cleanedData = res.replace(/\s/g, "");
+    return cleanedData.split(",");
+  };
 
+  const createNode = ({ position, type, label, model }) => {
+    const nodeId = uuidv4();
+    const newNode = {
+      id: nodeId,
+      position,
+      data: {
+        label,
+        model,
+      },
+      type,
+    };
+    return newNode;
+  };
+
+  const createEdge = (source, target) => {
+    const edge = {
+      id: `${source}-${target}`,
+      source: source,
+      target: target,
+      type: "smoothstep",
+      style: {
+        strokeWidth: 1.5,
+        stroke: "#808080",
+      },
+      markerEnd: {
+        type: MarkerType.ArrowClosed,
+        width: 20,
+        height: 20,
+        color: "#808080",
+        strokeWidth: 1.5,
+      },
+    };
+
+    return edge;
+  };
+
+  const handleSubmitCreate = async () => {
+    if (aiEnabled) {
+      const result = await getAiWorkflow({ text: aiPromptData });
+      console.log(result);
+      const nodes = getNodesFromAiResponse(result.data.body.text);
+
+      if (modelsList && modelsList.body) {
+        const resultNodes = [];
+        const resultEdges = [];
+        let y = 300;
+        let x = 100;
+        let lastNode = createNode({
+          position: { x, y },
+          type: "startNode",
+          label: "Start",
+        });
+        resultNodes.push(lastNode);
+        nodes.forEach((chatGptGenNode) => {
+          x += 200;
+          const firstModelWithType = modelsList.body.find(
+            (item) => item.typeenum === chatGptGenNode
+          );
+          let currentNode = createNode({
+            position: { x, y },
+            type: chatGptGenNode,
+            label: firstModelWithType.type,
+            model: firstModelWithType,
+          });
+          resultNodes.push(currentNode);
+          let newEdge = createEdge(lastNode.id, currentNode.id);
+          resultEdges.push(newEdge);
+          lastNode = currentNode;
+        });
+        x += 200;
+        let endNode = createNode({
+          position: { x, y },
+          type: "endNode",
+          label: "End",
+        });
+        resultNodes.push(endNode);
+        let newEdge = createEdge(lastNode.id, endNode.id);
+        resultEdges.push(newEdge);
+        console.log(resultNodes);
+        console.log(resultEdges);
+
+        const payload = {
+          name,
+          config: {
+            nodes: resultNodes,
+            edges: resultEdges,
+          },
+        };
+        const workflowData = await saveWorkFlow(payload);
+        navigate(`/workflow/edit/${workflowData.data.body.id}`);
+      }
+    } else {
+      const workflowData = await saveWorkFlow({
+        name,
+        config: {
+          nodes: [],
+          edges: [],
+        },
+      });
+      navigate(`/workflow/edit/${workflowData.data.body.id}`);
+    }
     setOpenModal(false);
   };
 
